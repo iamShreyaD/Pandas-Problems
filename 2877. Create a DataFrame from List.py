e6bad27723f4a2bfeca5974df9a8bdf15fbf376a
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2025-12-14 23:30:14 +0530

    Create 2877. Create a DataFrame from List.py

---
+++ 2877. Create a DataFrame from List.py
@@ -0,0 +1,11 @@
+Write a solution to create a DataFrame from a 2D list called student_data. This 2D list contains the IDs and ages of some students.
+
+The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
+
+#return pd.DataFrame(student_data, columns=['student_id', 'age'])
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    column = ['student_id', 'age']
+    df = pd.DataFrame(student_data, columns=column)
+    return df
